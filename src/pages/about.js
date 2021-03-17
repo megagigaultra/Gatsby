642d25d2642b6fae3f622731da80a9c2599a9619
DIN <dindinmilou@gmail.com>
2021-03-17 22:15:28 +0100

    Shared Components
    Navigation

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,17 @@
 import { Link } from 'gatsby'
 import React from 'react'
 
+import Footer from '../components/footer'
+import Header from '../components/header'
+
 const AboutPage = () => {
   return(
     <div>
-      <h1>About Me</h1>
-      <p>Hi i'm James, and i'm learning to work with gatsby.</p>
-      <p>Contact me <Link to="/contact">here</Link></p>
+        <Header />
+        <h1>About Me</h1>
+        <p>Hi i'm James, and i'm learning to work with gatsby.</p>
+        <p>Contact me <Link to="/contact">here</Link></p>
+        <Footer />
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import Footer from '../components/footer'
+import Header from '../components/header'
+
 const ContactPage = () => {
   return(
     <div>
-      <h1>Contact</h1>
-      <p>Stay in touch with me via Twitter : <a href="https://twitter.com/din__dong" target="_blank">@din__dong</a></p>
+        <Header />
+        <h1>Contact</h1>
+        <p>Stay in touch with me via Twitter : <a href="https://twitter.com/din__dong" target="_blank">@din__dong</a></p>
+        <Footer />
     </div>
   )
 }
